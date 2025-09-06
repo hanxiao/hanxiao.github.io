f210c009d2b0ddf242c0a0705ec65a869b44cc26
hanxiao <hanxiao@users.noreply.github.com>
2025-09-06 18:59:27 +0000

    deploy: hanxiao/blog@550032665ee6515d24a839a2d8c0744397160b40

---
+++ js/katexrender.js
@@ -0,0 +1,11 @@
+renderMathInElement(
+    document.body,
+    {
+        delimiters: [
+            {left: "$$", right: "$$", display: true},
+            {left: "\\[", right: "\\]", display: true},
+            {left: "$", right: "$", display: false},
+            {left: "\\(", right: "\\)", display: false}
+        ]
+    }
+);
